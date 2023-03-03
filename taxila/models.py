2808bb1e29e018f7a8d0dd891d4262431401a063
Sagar Yadav <sagaryadav17@outlook.com>
2023-03-03 11:00:33 +0530

    removed unneccessary code

--- taxila/models.py
+++ taxila/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from cache_keys import delete_all_cache
+from django.core.cache import cache
 from config.utils import get_uuid_filename
 
 
@@ -18,53 +18,50 @@ def kitchen_item_upload_to(instance, filename):
     return f"KitchenItemImage/{new_filename}"
 
 
-class KitchenCategory(models.Model):
-    ranking = models.PositiveIntegerField(default=0)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class MaterialCategory(models.Model):
-    ranking = models.PositiveIntegerField(default=0)
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class MaterialVendor(models.Model):
-    name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class Material(models.Model):
-    slug = models.SlugField(max_length=255, blank=True, null=True)
+class TimestampedModel(models.Model):
     ranking = models.PositiveBigIntegerField(default=0)
+
+    # A timestamp representing when this object was created.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # A timestamp representing when this object was last updated.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("ranking",)
+
+    def save(self, *args, **kwargs) -> None:
+        cache.clear()
+        return super().save(*args, **kwargs)
+
+
+class KitchenCategory(TimestampedModel):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MaterialCategory(TimestampedModel):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class MaterialVendor(TimestampedModel):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Material(TimestampedModel):
+    slug = models.SlugField(max_length=255, blank=True, null=True)
     name = models.CharField(max_length=255)
     images = models.TextField(blank=True, null=True)
     origin_country = models.CharField(max_length=255, blank=True, null=True)
@@ -77,34 +74,19 @@ class Material(models.Model):
     vendor = models.ForeignKey(MaterialVendor, on_delete=models.CASCADE, blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
     def __str__(self) -> str:
         return self.name
 
 
-class MaterialImage(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class MaterialImage(TimestampedModel):
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
     image = models.FileField(upload_to=material_upload_to)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return "%s - %s" % (self.item.name, self.id)
 
 
-class MaterialFeature(models.Model):
+class MaterialFeature(TimestampedModel):
     material = models.OneToOneField(Material, on_delete=models.CASCADE)
     chip = models.TextField(blank=True, null=True)
     heat = models.TextField(blank=True, null=True)
@@ -113,15 +95,11 @@ class MaterialFeature(models.Model):
     water = models.TextField(blank=True, null=True)
     frost = models.TextField(blank=True, null=True)
 
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
     def __str__(self):
         return "%s" % (self.material.name)
 
 
-class MaterialApplication(models.Model):
+class MaterialApplication(TimestampedModel):
     material = models.OneToOneField(Material, on_delete=models.CASCADE)
     countertops = models.TextField(blank=True, null=True)
     floorings = models.TextField(blank=True, null=True)
@@ -130,15 +108,11 @@ class MaterialApplication(models.Model):
     fireplace = models.TextField(blank=True, null=True)
     outdoor = models.TextField(blank=True, null=True)
 
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
     def __str__(self):
         return "%s" % (self.material.name)
 
 
-class MaterialTechnicalSpecification(models.Model):
+class MaterialTechnicalSpecification(TimestampedModel):
     material = models.OneToOneField(Material, on_delete=models.CASCADE)
     petrographic_denomination = models.TextField(blank=True, null=True)
     hardness = models.TextField(blank=True, null=True)
@@ -149,16 +123,11 @@ class MaterialTechnicalSpecification(models.Model):
     compressive_strength = models.TextField(blank=True, null=True)
     attachement = models.FileField(upload_to=material_technical_specification_upload_to)
 
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
     def __str__(self):
         return "%s" % (self.material.name)
 
 
-class KitchenItem(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class KitchenItem(TimestampedModel):
     order = models.CharField(max_length=255, blank=True, null=True)
     category = models.ForeignKey(KitchenCategory, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -171,97 +140,50 @@ class KitchenItem(models.Model):
     other = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
-
     def __str__(self):
         return "%s - %s" % (self.name, self.category.name)
 
 
-class KitchenItemImage(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class KitchenItemImage(TimestampedModel):
     item = models.ForeignKey(KitchenItem, on_delete=models.CASCADE)
     image = models.FileField(upload_to=kitchen_item_upload_to)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return "%s - %s" % (self.item.name, self.id)
 
 
-class InspirationCategory(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class InspirationCategory(TimestampedModel):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Inspiration(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class Inspiration(TimestampedModel):
     category = models.ForeignKey(InspirationCategory, on_delete=models.CASCADE)
     main_image = models.FileField(upload_to="")
     sub_image = models.FileField(upload_to="")
     title = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
-    is_active = models.BooleanField(is_active=True)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return "%s - %s - %s" % (self.id, self.category.name, self.title)
 
 
-class VideoCategory(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class VideoCategory(TimestampedModel):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.name
 
 
-class Video(models.Model):
-    ranking = models.PositiveBigIntegerField(default=0)
+class Video(TimestampedModel):
     category = models.ForeignKey(VideoCategory, on_delete=models.CASCADE)
     video_url = models.URLField(max_length=255)
     is_active = models.BooleanField(default=True)
-
-    class Meta:
-        ordering = ("ranking",)
-
-    def save(self, *args, **kwargs) -> None:
-        delete_all_cache()
-        return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return self.name
--- taxila/views.py
+++ taxila/views.py
@@ -19,12 +19,11 @@ from taxila.models import (
     MaterialCategory,
     MaterialVendor,
 )
-from taxila.cache_keys import CacheKeys
 
 
 class HomepageAPIView(APIView):
     # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.homepage.value))
+    @method_decorator(cache_page(60 * 15))
     def get(self, request):
         data = {}
         data["kitchen_category"] = KitchenCategory.objects.filter(is_active=True).values()
@@ -37,59 +36,56 @@ class KitchenCategoryListView(ListAPIView):
     serializer_class = KitchenCategorySerializer
     queryset = KitchenCategory.objects.filter(is_active=True)
 
-    # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.kitchen_category.value))
-    def get(self, *args, **kwargs):
-        return super().get(*args, **kwargs)
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class MaterialCategoryListView(ListAPIView):
     serializer_class = MaterialCategorySerializer
     queryset = MaterialCategory.objects.filter(is_active=True)
 
-    # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.material_category.value))
-    def get(self, *args, **kwargs):
-        return super().get(*args, **kwargs)
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class MaterialVendorListView(ListAPIView):
     serializer_class = MaterialVendorSerializer
     queryset = MaterialVendor.objects.filter(is_active=True)
 
-    # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.material_vendor.value))
-    def get(self, *args, **kwargs):
-        return super().get(*args, **kwargs)
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class MaterialListView(ListAPIView):
     serializer_class = MaterialSerializer
     queryset = Material.objects.filter(is_active=True, category__is_active=True, vendor__is_active=True)
 
-    # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.material.value))
-    def get(self, *args, **kwargs):
-        return super().get(*args, **kwargs)
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class KitchenItemListView(ListAPIView):
     serializer_class = KitchenItemSerializer
     queryset = KitchenItem.objects.filter(is_active=True, category__is_active=True)
 
-    # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.kitchen_category.value))
-    def get(self, *args, **kwargs):
-        return super().get(*args, **kwargs)
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class InspirationListView(ListAPIView):
     serializer_class = MaterialCategorySerializer
     queryset = Inspiration.objects.filter(is_active=True, category__is_active=True)
 
-    # Cache page for the requested url
-    @method_decorator(cache_page(60 * 15, key_prefix=CacheKeys.inspiration.value))
     def get(self, *args, **kwargs):
         data = super().get(*args, **kwargs)
         data["category"] = InspirationCategory.objects.filter(is_active=True).values()
         return data
+
+    @method_decorator(cache_page(60 * 15))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
